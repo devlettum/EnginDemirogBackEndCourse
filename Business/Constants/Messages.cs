acd4a8e184e0b9e2c5bb5e1a6e8b731e1684ac95
Batuhan Arık <batuhanarik123@gmail.com>
2021-03-07 21:56:33 +0300

    14.gün ders sonu kodları

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,5 +15,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError="Ürün sayısı 10 veya 10'dan az olmalı!";
         public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
         public static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied="Yetkiniz yok";
+        public static string AccessTokenCreated="Access Token Oluşturuldu";
+        public static string UserNotFound="Kullanıcı bulunamadı";
+        public static string PasswordError="Şifre hatalı!";
+        public static string SuccessfulLogin="Giriş başarılı";
+        public static string UserRegistered="Kayıt başarılı";
+        public static string UserAlreadyExists="Kullanıcı zaten var!";
     }
 }
